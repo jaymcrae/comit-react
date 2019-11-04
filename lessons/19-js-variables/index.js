8b61f28e268e82ac08665664ec2001f0be5bb68a
jaymcrae <jasonjdmcrae@gmail.com>
2019-11-03 21:37:18 -0600

    completes 19-js-variables.

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,11 @@
+const monthOfBirth="January";
+const firstName="Jason";
+const lastName="McRae";
+let city;
+let age;
+let nextCity;
+city="Saskatoon";
+age="40";
+nextCity=city;
+console.log('month of birth: ', monthOfBirth);
+console.log(`full name: ${firstName},${lastName}`);
